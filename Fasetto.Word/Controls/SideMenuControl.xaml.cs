10de75c14deecb6598bf91561ec40c25ffc16a37
Mitos Kalandiel <mitoskalandiel@gmail.com>
2017-10-06 11:48:41 +0100

    Sidebar created * We've learned about user controls * Created a control template to then inject the rest of the sidebar, once we've created the top menu of the sidebar * Used and implemented FontAwesome Glyphs to make the top menu items

---
+++ Fasetto.Word/Controls/SideMenuControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for SideMenuControl.xaml
+    /// </summary>
+    public partial class SideMenuControl : UserControl
+    {
+        #region Public Constructors
+
+        public SideMenuControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+    }
+}
