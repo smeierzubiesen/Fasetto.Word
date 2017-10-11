65bb339cbca220826da9e345e57c2d5eb6ab00e4
Mitos Kalandiel <mitoskalandiel@gmail.com>
2017-10-11 20:45:46 +0100

    created a chat message control that contains individual ChatMessageListItems, which we have created the layout for, with hardcoded content, atm. * created additional fontsizes and reorganised the resource dictionaries * moved ChatList and ChatMessage into their own directories.

---
+++ Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListControl.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for ChatMessageListControl.xaml
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        #region Public Constructors
+
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+    }
+}
