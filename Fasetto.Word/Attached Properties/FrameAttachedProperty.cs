157a60445601e8434893d245e2cfc92cb2ca4e0c
Mitos Kalandiel <mitoskalandiel@gmail.com>
2017-10-11 11:09:20 +0100

    Design Time & Navigation bar * fixed design time issues by introducing one additional layer of DataContext binding * introduced attachedproperty for navigation bar and history to set true/false

---
+++ Fasetto.Word/Attached Properties/FrameAttachedProperty.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace Fasetto.Word
+{
+    using System.Windows.Controls;
+
+    /// <inheritdoc/>
+    /// <summary>
+    /// The NoFrameHistory attached property for creating a <see cref="Frame"/> that never show
+    /// navigation (or behaviour)
+    /// </summary>
+    public class NoFrameHistory : BaseAttachedProperty<NoFrameHistory, bool>
+    {
+        #region Public Methods
+
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var frame = (sender as Frame);
+
+            // Hide the naviation bar
+            frame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+
+            // Clear history on navigate
+            frame.Navigated += (ss, ee) => ((Frame)ss).NavigationService.RemoveBackEntry();
+        }
+
+        #endregion Public Methods
+    }
+}
