006bfada53cf331ffb7c7f11eb272d562102b8d6
Mitos Kalandiel <mitoskalandiel@gmail.com>
2017-11-01 19:50:47 +0000

    lesson 14 finished. had to retrace a few steps from 2 weeks ago, as the app had broke during lesson 15 and I hadn't quite understood what had happened.

---
+++ Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for ChatMessageListControl.xaml
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// The ChatList Control Constructor
+        /// </summary>
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+    }
+}
---
+++ Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Fasetto.Word
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ChatMessageListItemControl.xaml
+    /// </summary>
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// The ChatMessage List Item control constructor
+        /// </summary>
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+    }
+}
