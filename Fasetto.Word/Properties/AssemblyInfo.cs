87d5bee247b5508d442cfac759d74f7d140846eb
Mitos Kalandiel <mitoskalandiel@gmail.com>
2017-11-02 12:23:07 -0700

    Added updated docs from doxygen/sphinx for version $($env:appveyor_build_version)

--- Fasetto.Word/Properties/AssemblyInfo.cs
+++ Fasetto.Word/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Windows;
 // Major Version Minor Version Build Number Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
-// as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// as shown below: [assembly: AssemblyVersion("0.15.92")]
+[assembly: AssemblyVersion("0.15.92")]
+[assembly: AssemblyFileVersion("0.15.92")]
