4b7aeda005ef1f652c106f86c6eaeb18328d390d
Mitos Kalandiel <mitoskalandiel@gmail.com>
2017-11-03 09:25:28 +0000

    removed everything to do with documentation as RTD does this now updated sphinx config to use RTD theme removed spurious cache entries for appveyor adapted some path entry for sphinx as well

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -97,7 +97,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -115,12 +115,12 @@ html_static_path = ['_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
+#html_sidebars = {
+#    '**': [
+#        'relations.html',  # needs 'show_related': True theme option to display
+#        'searchbox.html',
+#    ]
+#}
 
 
 # -- Options for HTMLHelp output ------------------------------------------
