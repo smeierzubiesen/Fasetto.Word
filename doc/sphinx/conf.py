7cca896280aae44c7663dc7e03ecdfdf0766c26e
Mitos Kalandiel <mitoskalandiel@gmail.com>
2017-11-03 08:35:46 +0000

    removed all doc generation from appveyor as this will be handled by RTD also adapted paths to be used on linux.

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -37,7 +37,7 @@ if read_the_docs_build:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.todo', 'sphinx.ext.githubpages', 'breathe']
-breathe_projects = { "FasettoWord": "c:\\Projects\\fasseto-word\\doc\\doxy\\xml\\" }
+breathe_projects = { "FasettoWord": "../doxy/xml/" }
 breathe_default_project = "FasettoWord"
 
 # Add any paths that contain templates here, relative to this directory.
@@ -181,4 +181,4 @@ texinfo_documents = [
 
 # Where to read the documentation from
 # In this case we already have documentation from doxygen
-html_extra_path = ['..\\doxy\\xml\\']
+#html_extra_path = ['..\\doxy\\xml\\']
